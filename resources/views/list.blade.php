25780542ec48f018459dcac3be287fc1a51a3336
多田昂央 <takass0119@yahoo.co.jp>
2019-11-14 22:23:22 +0900

    アイテム一覧画面実装

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,6 +1,163 @@
+<!doctype html>
+<html lang="ja">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    </head>
+
+    <body>
+    
+        <!-- ヘッダ -->
+         <div class="navbar navbar-dark shadow-sm" style="background-color: #46a032;">
+            <div class="container d-flex justify-content-between">
+                <a href="#" class="navbar-brand d-flex">
+                    <h2><strong>E I M S </strong></h2>
+                </a>
+            </div>
+        </div>
+
+        <!-- ログインフォーム -->
+        <div class="container">
+            <h1 class="text-center">アイテム一覧</h1>
+            <div class="row justify-content-end">
+                <nav aria-label="Page navigation">
+                    <ul class="pagination">
+                        <li class="page-item">
+                            <a class="page-link" href="#" aria-label="Previous">
+                                <span aria-hidden="true">&laquo;</span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item">
+                            <a class="page-link" href="#" aria-label="Next">
+                                <span aria-hidden="true">&raquo;</span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="row justify-content-center">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">カテゴリ</th>
+                            <th scope="col">アイテム</th>
+                            <th scope="col">数量</th>
+                            <th scope="col">使用期限</th>
+                            <th scope="col">警告</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>食品</td>
+                            <td>ｘｘｘｘｘｘｘｘｘｘｘｘｘｘｘ</td>
+                            <td>999</td>
+                            <td>1900/01/01</td>
+                            <td>－</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>食品</td>
+                            <td>ｘｘｘｘｘｘｘｘｘｘｘｘｘｘｘ</td>
+                            <td>999</td>
+                            <td>1900/01/01</td>
+                            <td>－</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>モバイルバッテリー</td>
+                            <td>ｘｘｘｘｘｘｘｘｘｘｘｘｘｘｘ</td>
+                            <td>999</td>
+                            <td>1900/01/01</td>
+                            <td>－</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">4</th>
+                            <td>食品</td>
+                            <td>ｘｘｘｘｘｘｘｘｘｘｘｘｘｘｘ</td>
+                            <td>999</td>
+                            <td>1900/01/01</td>
+                            <td>－</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">5</th>
+                            <td>食品</td>
+                            <td>ｘｘｘｘｘｘｘｘｘｘｘｘｘｘｘ</td>
+                            <td>999</td>
+                            <td>1900/01/01</td>
+                            <td>－</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">6</th>
+                            <td>モバイルバッテリー</td>
+                            <td>ｘｘｘｘｘｘｘｘｘｘｘｘｘｘｘ</td>
+                            <td>999</td>
+                            <td>1900/01/01</td>
+                            <td>－</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">7</th>
+                            <td>食品</td>
+                            <td>ｘｘｘｘｘｘｘｘｘｘｘｘｘｘｘ</td>
+                            <td>999</td>
+                            <td>1900/01/01</td>
+                            <td>－</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">8</th>
+                            <td>食品</td>
+                            <td>ｘｘｘｘｘｘｘｘｘｘｘｘｘｘｘ</td>
+                            <td>999</td>
+                            <td>1900/01/01</td>
+                            <td>－</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">9</th>
+                            <td>モバイルバッテリー</td>
+                            <td>ｘｘｘｘｘｘｘｘｘｘｘｘｘｘｘ</td>
+                            <td>999</td>
+                            <td>1900/01/01</td>
+                            <td>－</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">10</th>
+                            <td>モバイルバッテリー</td>
+                            <td>ｘｘｘｘｘｘｘｘｘｘｘｘｘｘｘ</td>
+                            <td>999</td>
+                            <td>1900/01/01</td>
+                            <td>－</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div>
+                <button type="submit" class="btn btn-primary">　＋　追加　</button>
+            </div>
+        </div>
+        
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    </body>
+</html>
+
+<!--
 <html>
     <body>
         <h1>List画面にようこそ、 {{ $name }}</h1>
         <h1> {{ $page }} ページ</h1>
     </body>
 </html>
+-->
