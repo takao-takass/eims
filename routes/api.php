ac8158f1858a2fbd1dc07c803d3fad5c5f602731
多田昂央 <takass0119@yahoo.co.jp>
2019-11-27 00:26:31 +0900

    ルーティングの整理

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,20 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
+
+# ログイン画面：認証API
+Route::post('/eims/login/auth','LoginController@auth');
+
+# アイテム登録画面：登録
+Route::post('/eims/new/entry','NewController@entry');
+
+# アイテム詳細画面：更新
+Route::post('/eims/detail/{id}/update','DetailController@update');
+
+# アイテム詳細画面：削除
+Route::post('/eims/detail/{id}/delete','DetailController@delete');
--- routes/web.php
+++ routes/web.php
@@ -19,23 +19,11 @@ Route::get('/', function () {
 Route::redirect('/eims', '/eims/login');
 Route::get('/eims/login','LoginController@index')->name('login');
 
-# ログイン画面：ログイン処理
-Route::post('/eims/login/auth','LoginController@auth');#->name('auth');
-
 # アイテム一覧：画面表示
 Route::get('/eims/list/{page}','ListController@index')->name('list');
 
 # アイテム登録画面：画面表示
 Route::get('/eims/new','NewController@index')->name('new');
 
-# アイテム登録画面：登録
-Route::post('/eims/new/entry','NewController@entry');#->name('entry');
-
 # アイテム詳細画面：画面表示
 Route::get('/eims/detail/{id}','DetailController@index')->name('detail');
-
-# アイテム詳細画面：更新
-Route::post('/eims/detail/{id}/update','DetailController@update');#->name('update');
-
-# アイテム詳細画面：削除
-Route::post('/eims/detail/{id}/delete','DetailController@delete');#->name('delete');
